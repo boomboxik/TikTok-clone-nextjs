0204b3e1df6be322bc030e45a694b9f4f121220b
boomboxik <9067506@gmail.com>
2023-11-25 23:44:11 +0300

    added icons

--- app/layouts/includes/TopNav.tsx
+++ app/layouts/includes/TopNav.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useRouter } from "next/router";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function TopNav() {
     const router = useRouter()
@@ -10,7 +9,7 @@ export default function TopNav() {
         <>
             <div id="TopNav" className="fixed bg-white z-30 flex items-center w-full border-b h-[60px]">
                  <div className={`flex items-center justify-between gap-6 w-full px-4 mx-auto ${pathname === '/' ? 'max-w-[1150px]' : ''}`}>
-                    
+
                  <Link href="/">
                         <img className="min-w-[115px] w-[115px]" src="/images/tiktok-logo.png"/>
                     </Link>
