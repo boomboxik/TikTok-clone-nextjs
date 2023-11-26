f5e95d9f1ae73e4af3b48f5de1ca6bdcd327cdde
boomboxik <9067506@gmail.com>
2023-11-26 20:00:54 +0300

    created a search bar

--- app/layouts/includes/TopNav.tsx
+++ app/layouts/includes/TopNav.tsx
@@ -5,6 +5,10 @@ export default function TopNav() {
     const router = useRouter()
     const pathname = usePathname()
 
+    const handleSearchName = (event: { target: { value: string } }) => {
+         console.log(event.target.value)
+    }
+
     return (
         <>
             <div id="TopNav" className="fixed bg-white z-30 flex items-center w-full border-b h-[60px]">
@@ -13,6 +17,15 @@ export default function TopNav() {
                  <Link href="/">
                         <img className="min-w-[115px] w-[115px]" src="/images/tiktok-logo.png"/>
                     </Link>
+
+                    <div className="relative hidden md:flex items-center justify-end bg-[#F1F1F2] p-1 rounded-full max-w-[430px] w-full">
+                      <input
+                         type="text"
+                         onChange={handleSearchName}
+                         className="w-full pl-3 my-2 bg-transparent placeholder-[#838383] text-[15px] focus:outline-none"
+                         placeholder="Search accounts"
+                         />
+                    </div>
 
                  </div>
             </div>
