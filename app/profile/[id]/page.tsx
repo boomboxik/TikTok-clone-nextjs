4f0f51e538b222fb95f59f318c79b01782908434
boomboxik <9067506@gmail.com>
2023-12-10 17:51:53 +0300

    created a profile page

---
+++ app/profile/[id]/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import MainLayout from "@/app/layouts/MainLayout";
+import { ProfilePageTypes } from "@/app/types";
+
+export default function Profile({ params }: ProfilePageTypes) {
+    return (
+        <>
+            <MainLayout>
+                <div>
+                    
+                </div>
+            </MainLayout>
+        </>
+    )
+}
