a336340e897bfce0f826477321173f8c9807aa9f
boomboxik <9067506@gmail.com>
2023-12-16 22:53:11 +0300

    update PostUser.tsx

--- app/components/profile/PostUser.tsx
+++ app/components/profile/PostUser.tsx
@@ -2,11 +2,13 @@ import { useEffect } from "react";
 import { PostUserCompTypes } from "@/app/types";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import Link from "next/link";
+import { SiSoundcharts } from "react-icons/si"
+import { BiErrorCircle } from "react-icons/bi";
 
 export default function PostUser ({ post }: PostUserCompTypes) {
 
   useEffect(() => {
-    const video = document.getElementById(`video-${post?.id}`) as HTMLVideoElement
+    const video = document.getElementById(`video${post.id}`) as HTMLVideoElement
 
     setTimeout(() => {
       video.addEventListener('mouseenter', () => { video.play() })
@@ -32,7 +34,17 @@ export default function PostUser ({ post }: PostUserCompTypes) {
                         src={post.video_url}
                     />
                 </Link>
-                )}
+            )}
+            <div className="px-1">
+                <p className="text-gray-700 text-[15px] pt-1 break-words">
+                    {post.text}
+                </p>
+                <div className="flex items-center gap-1 -ml-1 text-gray-600 font-bold text-xs">
+                    <SiSoundcharts size="15"/>
+                    3%
+                    <BiErrorCircle  size="16"/>
+                </div>
+            </div>
         </div>
       </>
   )
