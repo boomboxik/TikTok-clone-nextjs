9df7e7633513718dcc2e542feff6d1dc475eaa19
boomboxik <9067506@gmail.com>
2023-12-21 22:38:23 +0300

    update EditProfileOverlay.tsx

--- app/components/profile/EditProfileOverlay.tsx
+++ app/components/profile/EditProfileOverlay.tsx
@@ -40,6 +40,10 @@ export default function EditProfileOverlay() {
         return ''
     }
 
+    const cropAndUpdateImage = () => {
+        console.log("cropAndUpdateImage")
+    }
+
     return (
         <>
             <div
@@ -189,7 +193,23 @@ export default function EditProfileOverlay() {
                                 </button>
                             </div>
                         ) : (
-
+                            <div id="CropperButtons" className="flex items-center justify-end">
+                                <button 
+                                    onClick={() => setUploadedImage(null)}
+                                    className="flex items-center border rounded-sm px-3 py-[6px] hover:bg-gray-100"
+                                >
+                                    <span className="px-2 font-medium text-[15px]">Cancel</span>
+                                </button>
+                                
+                                <button 
+                                    onClick={() => cropAndUpdateImage()}
+                                    className="flex items-center bg-[#F02C56] text-white border rounded-md ml-3 px-3 py-[6px]"
+                                >
+                                    <span className="mx-4 font-medium text-[15px]">
+                                        {isUpdating ? <BiLoaderCircle color="#ffffff" className="my-1 mx-2.5 animate-spin" /> : "Apply" }
+                                    </span>
+                                </button>
+                            </div>
                         )}
                     </div>
                 </div>
