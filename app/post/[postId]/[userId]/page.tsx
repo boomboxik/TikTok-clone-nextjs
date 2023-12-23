038ca7c78209359b3758b36c11bfa1ef72e07c0d
boomboxik <9067506@gmail.com>
2023-12-23 19:50:26 +0300

    update user post page.tsx

--- app/post/[postId]/[userId]/page.tsx
+++ app/post/[postId]/[userId]/page.tsx
@@ -4,9 +4,18 @@ import { PostPageTypes } from "@/app/types";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { AiOutlineClose } from "react-icons/ai";
+import { BiChevronDown, BiChevronUp } from "react-icons/bi";
 
 export default function Post({ params }: PostPageTypes) {
     const router = useRouter
+
+    const loopThroughPostsUp = () => {
+        console.log('loopThroughPostsUp')
+    }
+
+    const loopThroughPostsDown = () => {
+        console.log('loopThroughPostsdown')
+    }
 
     return (
         <>
@@ -21,6 +30,22 @@ export default function Post({ params }: PostPageTypes) {
                     >
                         <AiOutlineClose size="27"/>
                     </Link>
+
+                    <div>
+                        <button 
+                            onClick={() => loopThroughPostsUp()}
+                            className="absolute z-20 right-4 top-4 flex items-center justify-center rounded-full bg-gray-700 p-1.5 hover:bg-gray-800"
+                        >
+                            <BiChevronUp size="30" color="#FFFFFF"/>
+                        </button>
+
+                        <button  
+                            onClick={() => loopThroughPostsDown()}
+                            className="absolute z-20 right-4 top-20 flex items-center justify-center rounded-full bg-gray-700 p-1.5 hover:bg-gray-800"
+                        >
+                            <BiChevronDown size="30" color="#FFFFFF"/>
+                        </button>
+                    </div>
                 </div>
             </div>
         </>
