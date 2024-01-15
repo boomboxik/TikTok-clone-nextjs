a82c3b0a33546caedec6c4d5f7f2102dfcb70da1
boomboxik <9067506@gmail.com>
2024-01-15 15:50:51 +0300

    final update for post components

--- app/components/post/SingleComment.tsx
+++ app/components/post/SingleComment.tsx
@@ -1,20 +1,34 @@
-import { SingleCommentCompTypes } from "@/app/types";
-import Link from "next/link";
-import { useState } from "react";
-import { BiLoaderCircle } from "react-icons/bi";
-import { BsTrash3 } from "react-icons/bs";
+import { useUser } from "@/app/context/user"
+import Link from "next/link"
+import { useState } from "react"
+import { BiLoaderCircle } from "react-icons/bi"
+import { BsTrash3 } from "react-icons/bs"
+import { useCommentStore } from "@/app/stores/comment"
+import moment from "moment"
+import useDeleteComment from "@/app/hooks/useDeleteComment"
+import useCreateBucketUrl from "@/app/hooks/useCreateBucketUrl"
+import { SingleCommentCompTypes } from "@/app/types"
 
 export default function SingleComment({ comment, params }: SingleCommentCompTypes) {
 
-    const [isDeleting, setIsDeleting] = useState<boolean>(false)
+    const contextUser = useUser()
+    let { setCommentsByPost } = useCommentStore()
+    const [isDeleting, setIsDeleting] = useState(false)
 
-    const deleteThisComment = () => {
-        let res = confirm("Are you sure you want to delete this comment?")
+    const deleteThisComment = async () => {
+        let res = confirm("Are you sure you weant to delete this comment?")
         if (!res) return
 
-        //do smthn 
+        try {
+            setIsDeleting(true)
+            await useDeleteComment(comment?.id)
+            setCommentsByPost(params?.postId)
+            setIsDeleting(false)
+        } catch (error) {
+            console.log(error)
+            alert(error)
+        }
     }
-
     return (
         <>
             <div id="SingleComment" className="flex items-center justify-between px-8 mt-4">
@@ -23,23 +37,24 @@ export default function SingleComment({ comment, params }: SingleCommentCompType
                         <img 
                             className="absolute top-0 rounded-full lg:mx-0 mx-auto" 
                             width="40" 
-                            src={comment.profile.image}
+                            src={useCreateBucketUrl(comment.profile.image)}
                         />
                     </Link>
                     <div className="ml-14 pt-0.5 w-full">
+
                         <div className="text-[18px] font-semibold flex items-center justify-between">
                             <span className="flex items-center">
                                 {comment?.profile?.name} - 
                                 <span className="text-[12px] text-gray-600 font-light ml-1">
-                                    {comment?.created_at}
+                                    {moment(comment?.created_at).calendar()}
                                 </span>
                             </span>
 
-                            {true ? (
+                            {contextUser?.user?.id == comment.profile.user_id ? (
                                 <button 
                                     disabled={isDeleting} 
                                     onClick={() => deleteThisComment()}
-                            >
+                                >
                                     {isDeleting 
                                         ? <BiLoaderCircle className="animate-spin" color="#E91E62" size="20"/>
                                         : <BsTrash3 className="cursor-pointer" size="25"/>
@@ -47,9 +62,9 @@ export default function SingleComment({ comment, params }: SingleCommentCompType
                                 </button>
                             ) : null}
                         </div>
-
-                        <p className="text-[15px] font-light">{comment.text}</p>
                         
+                        <p className="text-[15px] font-light">{comment.text}</p>
+
                     </div>
                 </div>
             </div>
