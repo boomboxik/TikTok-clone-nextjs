b80da17117dca99941ceb288d5a0a435b21a6295
boomboxik <9067506@gmail.com>
2024-01-19 14:58:14 +0300

    final update for page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,20 @@
 import MainLayout from "./layouts/MainLayout"
 import ClientOnly from "./components/ClientOnly"
 import PostMain from "./components/PostMain"
+import { useEffect } from "react";
+import { usePostStore } from "./stores/post";
 
 export default function Home() {
+  let { allPosts, setAllPosts } = usePostStore();
+  useEffect(() => { setAllPosts()}, [])
   return (
     <>
       <MainLayout>
         <div className="mt-[80px]  w-[calc(100%-90px)] max-w-[690px] ml-auto">
           <ClientOnly>
-            <PostMain post={{ 
-              id: '123',
-              user_id: '456',
-              video_url: '/budda.mp4',
-              text: 'some text',
-              created_at: 'date here',
-              profile: {
-                user_id: '456',
-                name: 'User 1',
-                image: 'https://placehold.co/100',
-              }
-            }}/>
+              {allPosts.map((post, index) => (
+              <PostMain post={post} key={index} />
+            ))}
           </ClientOnly>
         </div>
       </MainLayout>
